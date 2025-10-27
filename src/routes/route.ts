da6bf6ba22b34956ccbeb96d4e9661b21fbaed10
bakdoolot <nurisulajmanov.com>
2025-10-27 15:16:04 +0600

    added versel2

--- src/routes/route.ts
+++ src/routes/route.ts
@@ -1,12 +1,12 @@
-import cors from 'cors';
+import cors from "cors";
 import { Router } from "express";
 import todoRouter from "../modules/crud/crud.routes";
 
 const globalRouter = Router();
 
 const corsConfig = {
-  origin: ["http://localhost:3000", "https://bunch-to-backend-1.onrender.com/"]
-}
+  origin: ["http://localhost:3000", "https://bunch-to-backend.vercel.app/"],
+};
 
 globalRouter.use("/crud", cors(corsConfig), todoRouter);
 
